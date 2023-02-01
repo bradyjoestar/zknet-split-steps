fcd5fd4b0f983b2b265860b3cbe44f153810f98e
wenbin <bradyjoestar@gmail.com>
2023-02-01 18:13:48 +0800

    wb:add blockscout

--- splitdemo/1_download_project.sh
+++ splitdemo/1_download_project.sh
@@ -13,3 +13,4 @@ cd zkevm-node && git checkout wb/develop && cd ..
 
 ## change zkevm-contact's package.json for replacing zkevm-commonjs
 
+git clone git@github.com:bradyjoestar/blockscout.git
--- splitdemo/5_start_l1.sh
+++ splitdemo/5_start_l1.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+#
+#DOCKERCOMPOSE := docker-compose -f docker-compose.yml
+#DOCKERCOMPOSEAPPSEQ := zkevm-sequencer
+#DOCKERCOMPOSEAPPL2GASP := zkevm-l2gaspricer
+#DOCKERCOMPOSEAPPAGG := zkevm-aggregator
+#DOCKERCOMPOSEAPPRPC := zkevm-json-rpc
+#DOCKERCOMPOSEAPPSYNC := zkevm-sync
+#DOCKERCOMPOSEAPPBROADCAST := zkevm-broadcast
+#DOCKERCOMPOSEAPPETHTXMANAGER := zkevm-eth-tx-manager
+#DOCKERCOMPOSESTATEDB := zkevm-state-db
+#DOCKERCOMPOSEPOOLDB := zkevm-pool-db
+#DOCKERCOMPOSENETWORK := zkevm-mock-l1-network
+#DOCKERCOMPOSEEXPLORERL1 := zkevm-explorer-l1
+#DOCKERCOMPOSEEXPLORERL1DB := zkevm-explorer-l1-db
+#DOCKERCOMPOSEEXPLORERL2 := zkevm-explorer-l2
+#DOCKERCOMPOSEEXPLORERL2DB := zkevm-explorer-l2-db
+#DOCKERCOMPOSEEXPLORERRPC := zkevm-explorer-json-rpc
+#DOCKERCOMPOSEZKPROVER := zkevm-prover
+#DOCKERCOMPOSEZKPROVERMOCK := zkprover-mock
+#DOCKERCOMPOSEPERMISSIONLESSDB := zkevm-permissionless-db
+#DOCKERCOMPOSEPERMISSIONLESSNODE := zkevm-permissionless-node
+#DOCKERCOMPOSENODEAPPROVE := zkevm-approve
+#DOCKERCOMPOSEMETRICS := zkevm-metrics
+#DOCKERCOMPOSEGRAFANA := grafana
+
+
+
+
+
+docker-compose -f docker-compose.yml up -d zkevm-mock-l1-network
+
+docker-compose -f docker-compose.yml up -d zkevm-explorer-l1
+docker-compose -f docker-compose.yml up -d zkevm-explorer-l1-db
